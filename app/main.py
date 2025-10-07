b6f73481d34966e009c46952f3230da141d870c5
ma.parrat <ma.parrat@uniandes.edu.co>
2025-10-06 21:56:09 -0500

    Listar Provedores

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app.routers import proveedor_router
 
-app = FastAPI()
+# Crear tablas
+Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI on Cloud Run!"}
+app = FastAPI(title="MediSupply API", version="1.0.0")
+
+# Incluir rutas
+app.include_router(proveedor_router.router)
