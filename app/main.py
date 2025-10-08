1e9fdfc3df760f43b1e567a750c64c86f732a0e2
ma.parrat <ma.parrat@uniandes.edu.co>
2025-10-07 21:40:59 -0500

    Add cache and pagin

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routers import proveedor_router
-
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
 # Crear tablas
 Base.metadata.create_all(bind=engine)
 
@@ -9,3 +10,8 @@ app = FastAPI(title="MediSupply API", version="1.0.0")
 
 # Incluir rutas
 app.include_router(proveedor_router.router)
+
+# ✅ Initialize in-memory cache
+@app.on_event("startup")
+async def startup_event():
+    FastAPICache.init(InMemoryBackend(), prefix="fastapi-cache")
--- app/routers/proveedor_router.py
+++ app/routers/proveedor_router.py
@@ -1,8 +1,10 @@
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models.proveedor import Proveedor
 from app.schemas.proveedor_schema import ProveedorCreate, ProveedorOut
+from fastapi_cache.decorator import cache
 
 router = APIRouter(prefix="/proveedores", tags=["Proveedores"])
 
@@ -26,5 +28,6 @@ def crear_proveedor(proveedor: ProveedorCreate, db: Session = Depends(get_db)):
     return nuevo_proveedor
 
 @router.get("/", response_model=list[ProveedorOut])
-def listar_proveedores(db: Session = Depends(get_db)):
-    return db.query(Proveedor).all()
+@cache(expire=60)  # cache for 60 seconds
+def listar_proveedores(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return db.query(Proveedor).offset(skip).limit(limit).all()
