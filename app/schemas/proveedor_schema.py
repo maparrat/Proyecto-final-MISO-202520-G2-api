e2d8c24f9000d3190cbd10d806f758eb31d334ef
ma.parrat <ma.parrat@uniandes.edu.co>
2025-10-11 21:44:08 -0500

    Fix create

--- app/schemas/proveedor_schema.py
+++ app/schemas/proveedor_schema.py
@@ -4,10 +4,10 @@ from typing import Optional
 
 class CertificadoBase(BaseModel):
     nombre: str
-    cuerpoCertificador: str| None = None
-    fechaCertificacion: date| None = None
-    fechaVencimiento: date| None = None
-    urlDocumento: str| None = None
+    cuerpoCertificador: Optional[str] = None
+    fechaCertificacion: Optional[date] = None
+    fechaVencimiento: Optional[date] = None
+    urlDocumento: Optional[str] = None
 
 
 class CertificadoCreate(CertificadoBase):
@@ -23,16 +23,16 @@ class CertificadoOut(CertificadoBase):
 
 class ProveedorBase(BaseModel):
     nombre: str
-    id_tax: str | None = None
-    direccion: str | None = None
-    telefono: str | None = None
-    correo: str | None = None
-    contacto: str | None = None
-    estado: str | None = None
+    id_tax: Optional[str] = None
+    direccion: Optional[str] = None
+    telefono: Optional[str] = None
+    correo: Optional[str] = None
+    contacto: Optional[str] = None
+    estado: Optional[str] = None
 
 
 class ProveedorCreate(ProveedorBase):
-    certificado: Optional[CertificadoOut] = None
+    certificado: Optional[CertificadoCreate] = None
 
 
 class ProveedorOut(ProveedorBase):
